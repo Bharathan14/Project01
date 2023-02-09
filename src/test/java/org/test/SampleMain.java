df6b7b63bb6fc48b87266266927de12d2e6a4ab2
Bharathan14 <bharathoct14@gmail.com>
2023-02-09 19:33:53 +0530

    Bharath code

--- src/test/java/org/test/SampleMain.java
+++ src/test/java/org/test/SampleMain.java
@@ -2,7 +2,10 @@ package org.test;
 
 public class SampleMain {
 	public static void main(String[] args) {
-		
+		System.out.println("Bharathan");
+		System.out.println("Test");
+		System.out.println("Test1");
+		System.out.println("Print");
 	}
 
 }
